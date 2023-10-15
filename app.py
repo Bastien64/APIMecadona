22ee7a2e7deb3e620f596bc34c11d07cb9257691
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-10-15 23:46:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ import base64
 import bcrypt
 from flask_cors import CORS
 from werkzeug.serving import WSGIRequestHandler
-import io  
+import io  # Importez le module io
 from PIL import Image  
 from sqlalchemy.dialects.postgresql import NUMERIC
 from sqlalchemy.dialects.postgresql import BYTEA  
 from base64 import b64decode
-
 WSGIRequestHandler.protocol_version = "HTTP/1.1"
+
 app = Flask(__name__)
+
+# Utilisez os.environ.get pour obtenir le port de l'environnement Heroku
 port = int(os.environ.get('PORT', 5000))
+
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456Azerty!@37.187.39.204/studi'
+
 db = SQLAlchemy(app)
 
 class Admin(db.Model):
@@ -28,7 +32,7 @@ class Produit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    image = db.Column(BYTEA, nullable=False)  
+    image = db.Column(BYTEA, nullable=False)  # Utilisez BYTEA pour stocker les images
     categorie_id = db.Column(db.Integer, nullable=False)
 
 
@@ -47,7 +51,9 @@ class Promotion(db.Model):
 def login_admin():
     login = request.json.get('login')
     password = request.json.get('password')
+
     admin = Admin.query.filter_by(login=login).first()
+
     if admin:
         hashed_password = admin.password.encode('utf-8')
         if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
@@ -65,27 +71,37 @@ def login_admin():
 def create_admin():
     login = request.json.get('login')
     password = request.json.get('password')
+
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
     new_admin = Admin(login=login, password=hashed_password)
+
     db.session.add(new_admin)
     db.session.commit()
+
     return "Admin created successfully", 201
 
 @app.route('/produit', methods=['GET'])
 def get_produits():
     produits = Produit.query.all()
     produits_data = []
+
     for produit in produits:
+        # Convertir le champ "image" en une chaîne base64
         image_base64 = base64.b64encode(produit.image).decode('utf-8')
+
+        # Ajoutez le préfixe 'data:image/jpeg;base64,' à l'image
         image_base64_with_prefix = 'data:image/jpeg;base64,' + image_base64
+
         produit_data = {
             'id': produit.id,
             'description': produit.description,
-            'price': produit.price, 
-            'image': image_base64_with_prefix, 
+            'price': produit.price,  # Convertir le price en chaîne de caractères
+            'image': image_base64_with_prefix,  # Champ "image" avec préfixe
             'categorie_id': produit.categorie_id
         }
         produits_data.append(produit_data)
+
     return jsonify(produits_data)
 
 
@@ -95,15 +111,23 @@ def get_produits():
 def create_produit():
     description = request.json.get('description')
     price = request.json.get('price')
-    image_base64 = request.json.get('image')
-    categorie_id = request.json.get('categorie_id') 
+    image_base64 = request.json.get('image')  # Récupérez l'image en base64
+    categorie_id = request.json.get('categorie_id')  # Récupérez categorie_id
+    # Vérifiez si l'image contient déjà le préfixe 'data:image/jpeg;base64,'
     if not image_base64.startswith('data:image/jpeg;base64,'):
         image_base64 = 'data:image/jpeg;base64,' + image_base64
+
+    # Supprimez le préfixe pour obtenir les données brutes en base64
     image_data = image_base64.replace('data:image/jpeg;base64,', '')
+
+    # Convertissez les données base64 en bytes
     image_bytes = b64decode(image_data)
+
     new_produit = Produit(description=description, price=price, image=image_bytes, categorie_id=categorie_id)
+
     db.session.add(new_produit)
     db.session.commit()
+
     return "Produit créé avec succès", 201
 
 
@@ -111,12 +135,14 @@ def create_produit():
 def get_categories():
     categories = Categorie.query.all()
     categories_data = []
+
     for categorie in categories:
         categorie_data = {
             'id': categorie.id,
             'libelle': categorie.libelle
         }
         categories_data.append(categorie_data)
+
     return jsonify(categories_data)
 
 @app.route('/promotion', methods=['GET'])
@@ -133,22 +159,28 @@ def get_promotions():
             'produit_id': promotion.produit_id
         }
         promotions_data.append(promotion_data)
+
     return jsonify(promotions_data)
 
 @app.route('/promotion', methods=['POST'])
 def add_promotion():
     data = request.get_json()
+
     if 'datedebut' in data and 'datefin' in data and 'pourcentage' in data and 'produit_id' in data:
         datedebut = data['datedebut']
         datefin = data['datefin']
         pourcentage = data['pourcentage']
         produit_id = data['produit_id']
+
         new_promotion = Promotion(datedebut=datedebut, datefin=datefin, pourcentage=pourcentage, produit_id=produit_id)
+
         db.session.add(new_promotion)
         db.session.commit()
+
         return jsonify({"message": "La promotion a été ajoutée avec succès."}), 201
     else:
         return jsonify({"error": "Toutes les données nécessaires ne sont pas fournies."}), 400
 
 if __name__ == '__main__':
+    # Utilisez le port défini par Heroku (via os.environ.get)
     app.run(host="0.0.0.0", port=port)
